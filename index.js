205b8cedb7f742801e1c011741ae2ee1d0b74282
nikolaskorakidis <korakidisn@gmail.com>
2021-07-28 11:31:50 +0200

    removing JS

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const btn = document.querySelector("button");
+const images = document.querySelectorAll(".product-image .inner");
 
-btn.addEventListener("click", () => alert("Congratulations"));
+images.forEach((i) => {
+  const firstImg = i.querySelector(".wp-post-image");
+  const secondImg = i.querySelector(".hover-image");
+
+  if (
+    !!firstImg &&
+    !!secondImg &&
+    firstImg.classList.contains("wp-post-image") &&
+    secondImg.classList.contains("hover-image")
+  ) {
+    firstImg.classList.remove("wp-post-image");
+    firstImg.classList.add("hover-image");
+
+    secondImg.classList.remove("hover-image");
+    secondImg.classList.add("wp-post-image");
+  }
+});
